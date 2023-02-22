8e7296e92f6abe2378c06f82b44284cb0cfb7b92
ZEugenii <zinich911@gmail.com>
2023-02-22 15:04:45 +0200

    connection mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Yevhenii:MongoCreateDb123@cluster0.nwubwpa.mongodb.net/contact_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
