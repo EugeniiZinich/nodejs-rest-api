348bd5dbfda056389bd5e0b13464a70e1cad6942
ZEugenii <zinich911@gmail.com>
2023-03-07 15:55:50 +0200

    fix authenticate.js file

--- middlewars/authenticate.js
+++ middlewars/authenticate.js
@@ -20,7 +20,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
 
     const user = await User.findById(id);
-    console.log(user);
+
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
